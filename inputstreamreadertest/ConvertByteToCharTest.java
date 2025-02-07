703245ea6f24464b3c9a88eba1da265f7ff3d15e
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-07 16:48:03 +0530

    [Shruti] Add. Convert byte stream to char stream and user input in a file is tested

---
+++ inputstreamreadertest/ConvertByteToCharTest.java
@@ -0,0 +1,12 @@
+package inputstreamreadertest;
+import com.java.inputstreamreader.ConvertByteToChar;
+import org.junit.Test;
+import org.junit.Assert;
+public class ConvertByteToCharTest {
+    @Test
+    public void byteToChar(){
+        String[] args = {};
+        ConvertByteToChar.main(args);
+    }
+
+}
---
+++ inputstreamreadertest/UserInputTest.java
@@ -0,0 +1,12 @@
+package inputstreamreadertest;
+import com.java.inputstreamreader.UserInput;
+import org.junit.Test;
+import org.junit.Assert;
+public class UserInputTest {
+    @Test
+    public void userCheck(){
+        String[] argument = {};
+        UserInput.main(argument);
+    }
+
+}
