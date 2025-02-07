3060fec7c08a7c8908e298297c167cd465894e26
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-02-07 16:47:29 +0530

    [Shruti] Add. ReadFile and WordOccurrence in a file is tested

---
+++ filereadertest/ReadFileTest.java
@@ -0,0 +1,14 @@
+package filereadertest;
+
+import com.java.filereader.ReadFile;
+import org.junit.Test;
+import org.junit.Assert;
+public class ReadFileTest {
+
+    @Test
+    public void readFileTest(){
+        String[] str = {};
+        ReadFile.main(str);
+    }
+
+}
---
+++ filereadertest/WordOccurrenceTest.java
@@ -0,0 +1,11 @@
+package filereadertest;
+import com.java.filereader.WordOccurrence;
+import org.junit.Test;
+import org.junit.Assert;
+public class WordOccurrenceTest {
+    @Test
+    public void wordOccurrenceTest(){
+        String [] str = {};
+        WordOccurrence.main(str);
+    }
+}
